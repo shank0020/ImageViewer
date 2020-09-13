71c34b54bc2438ca0fa347118a073d974a1d3712
shank0020 <shashank.cse20@gmail.com>
2020-09-14 04:55:26 +0530

    Setting url constants

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,4 +1,4 @@
-export const constants = {
-    userInfoUrl:"https://api.instagram.com/v1/users/self",
-    userMediaUrl:"https://api.instagram.com/v1/users/self/media/recent"
-  }
+export const constants = { 
+  userInfoUrl:"https://graph.instagram.com/244826603501048/media?fields=username,caption,id,media_type,timestamp,permalink,media_url,thumbnail_url",
+  userMediaUrl:"https://graph.instagram.com/me/media?fields=username,caption,id,media_type,timestamp,permalink,media_url,thumbnail_url"
+}
